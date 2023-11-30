86c6825d8faec2b032dd8bbb2a61bad75a6e185e
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-11-30 16:39:48 -0300

    (models): Add models using pydantic

---
+++ models/planet/planet.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class PlanetModel(BaseModel):
+    name: str
+    rotation_period: Optional[str] = None
+    orbital_period: Optional[str] = None
+    diameter: Optional[str] = None
+    climate: Optional[str] = None
+    gravity: Optional[str] = None
+    terrain: Optional[str] = None
+    surface_water: Optional[str] = None
+    population: Optional[str] = None
+    residents: Optional[list] = []
+    films: Optional[list] = []
+    created: Optional[str] = None
+    edited: Optional[str] = None
+    url: Optional[str] = None
