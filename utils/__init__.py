12f64817ffddc9eb247d5fa21917c1411a73ff61
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-11-30 17:08:59 -0300

    (utils/): Remove unecessary code

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,2 @@
 from .utils import get_list_all_elements 
-from .utils import export_person_to_csv
-from .utils import export_planet_to_csv
-from .utils import export_starship_to_csv
+from .utils import export_generic_model_to_csv
--- utils/utils.py
+++ utils/utils.py
@@ -2,9 +2,7 @@ import requests
 import logging
 import csv
 from pydantic import BaseModel
-from models.person import PersonModel
-from models.planet import PlanetModel
-from models.starship import StarShipModel
+
 
 def get_list_all_elements(resource: str) -> list[dict]:
     base_url = "https://swapi.dev/api"
@@ -20,38 +18,6 @@ def get_list_all_elements(resource: str) -> list[dict]:
         except Exception as e:
             logging.error(f"An error: {e}")
     return list_of_elements
-
-
-def export_person_to_csv(list_of_elements_to_be_exported: list[dict], exported_file_name: str) -> str:
-    with open(exported_file_name, "w", newline='') as myfile:
-        writer = csv.DictWriter(myfile, fieldnames=PersonModel.model_fields.keys())
-        writer.writeheader()
-        for dict_element in list_of_elements_to_be_exported:
-            person_instance = PersonModel(**dict_element)
-            person_dict = person_instance.model_dump()
-            writer.writerow(person_dict)
-    logging.info("The csv file has been exported")
-
-
-def export_planet_to_csv(list_of_elements_to_be_exported: list[dict], exported_file_name: str) -> str:
-    with open(exported_file_name, "w", newline='') as myfile:
-        writer = csv.DictWriter(myfile, fieldnames=PlanetModel.model_fields.keys())
-        writer.writeheader()
-        for dict_element in list_of_elements_to_be_exported:
-            planet_instance = PlanetModel(**dict_element)
-            planetc_dict = planet_instance.model_dump()
-            writer.writerow(planetc_dict)
-    logging.info("The csv file has been exported")
-
-def export_starship_to_csv(list_of_elements_to_be_exported: list[dict], exported_file_name: str) -> str:
-    with open(exported_file_name, "w", newline='') as myfile:
-        writer = csv.DictWriter(myfile, fieldnames=StarShipModel.model_fields.keys())
-        writer.writeheader()
-        for dict_element in list_of_elements_to_be_exported:
-            starship_instance = StarShipModel(**dict_element)
-            starship_dict = starship_instance.model_dump()
-            writer.writerow(starship_dict)
-    logging.info("The csv file has been exported")
 
 
 def export_generic_model_to_csv(model_class: BaseModel, list_of_elements_to_be_exported: list[dict], exported_file_name: str) -> str:
