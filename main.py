a885152c01db88e4bb9c802da8e4f1bd1b611c9a
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-11-30 14:09:16 -0300

    (main.py): Create the export to csv script

---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+import logging
+import csv
+
+def get_list_all_elements(resource: str) -> list[dict]:
+    base_url = "https://swapi.dev/api"
+    url = f"{base_url}{resource}?page=1"
+    list_of_elements = []
+    while url:
+        try:
+            response = requests.get(url)
+            data = response.json()
+            url = data.get("next")
+            list_of_elements.extend(data["results"])
+            logging.info(f"The next url is {url}")
+        except Exception as e:
+            logging.error(f"An error: {e}")
+    return list_of_elements
+
+
+def export_to_csv(list_of_elements_to_be_exported: list[dict], exported_file_name: str) -> str:
+    myfile = open(exported_file_name, "w")
+    writer = csv.writer(myfile)
+    writer.writerow((list_of_elements_to_be_exported[0].keys()))
+    for dict_element in list_of_elements_to_be_exported:
+        writer.writerow(dict_element.values())
+    myfile.close()
+    return "The csv file has been exported"
+
+
+if __name__ == "__main__":
+    resource_and_filename = [
+    {
+        "resource": "/people/",
+        "filename": "personagens.csv"
+    },
+    {
+        "resource": "/planets/",
+        "filename": "planetas.csv"
+    },
+    {
+        "resource": "/starships/",
+        "filename": "starships.csv"
+    }
+    ]
+    for dicio in resource_and_filename:
+        try:
+            list_of_element = get_list_all_elements(resource=dicio["resource"])
+            export_to_csv(
+                list_of_elements_to_be_exported=list_of_element,
+                exported_file_name=dicio["filename"]
+            )
+            logging.info(f"The data of resource {dicio['resource']} has been colected and the file {dicio['filename']} has been exported!")
+        except Exception as e:
+            logging.error(f"An error: {e}")
