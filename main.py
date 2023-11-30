0b4ce9ed4e69af17e25aece15d76662a463ebaab
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-11-30 16:41:51 -0300

    (main.py): Import each function to be called for respective module schema

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import logging
-from utils import get_list_all_elements, export_to_csv
+from utils import get_list_all_elements, export_person_to_csv, export_planet_to_csv, export_starship_to_csv
 
 
 if __name__ == "__main__":
-    resource_and_filename = [
-    {
-        "resource": "/people/",
-        "filename": "personagens.csv"
-    },
-    {
-        "resource": "/planets/",
-        "filename": "planetas.csv"
-    },
-    {
-        "resource": "/starships/",
-        "filename": "starships.csv"
-    }
-    ]
-    for dicio in resource_and_filename:
-        try:
-            list_of_element = get_list_all_elements(resource=dicio["resource"])
-            export_to_csv(
-                list_of_elements_to_be_exported=list_of_element,
-                exported_file_name=dicio["filename"]
-            )
-            logging.info(f"The data of resource {dicio['resource']} has been colected and the file {dicio['filename']} has been exported!")
-        except Exception as e:
-            logging.error(f"An error: {e}")
+    try:
+        list_of_element = get_list_all_elements(resource="/people/")
+        export_person_to_csv(
+            list_of_elements_to_be_exported=list_of_element,
+            exported_file_name="personagens.csv"
+        )
+        logging.info(f"The data of resource '/people/' has been colected and the file 'personagens.csv' has been exported!")
+    except Exception as e:
+        logging.error(f"An error: {e}")
+    
+    try:
+        list_of_element = get_list_all_elements(resource="/planets/")
+        export_planet_to_csv(
+            list_of_elements_to_be_exported=list_of_element,
+            exported_file_name="planetas.csv"
+        )
+        logging.info(f"The data of resource '/planets/' has been colected and the file 'planetas.csv' has been exported!")
+    except Exception as e:
+        logging.error(f"An error: {e}")
+    
+    try:
+        list_of_element = get_list_all_elements(resource="/starships/")
+        export_starship_to_csv(
+            list_of_elements_to_be_exported=list_of_element,
+            exported_file_name="starships.csv"
+        )
+        logging.info(f"The data of resource '/starships/' has been colected and the file 'starships.csv' has been exported!")
+    except Exception as e:
+        logging.error(f"An error: {e}")
