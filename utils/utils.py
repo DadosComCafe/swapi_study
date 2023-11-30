f13b111c82e497642e0822d86c733883e40259a3
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-11-30 17:41:21 -0300

    (utils/utils.py): Apply docstring in utils functions

--- utils/utils.py
+++ utils/utils.py
@@ -5,6 +5,14 @@ from pydantic import BaseModel
 
 
 def get_list_all_elements(resource: str) -> list[dict]:
+    """Get a list of dictionaries from the given endpoint by resource.
+
+    Args:
+        resource (str): The path of endpoint where you want to make the get request to retrieve the data.
+
+    Returns:
+        list[dict]: List of dictionary from the api.
+    """
     base_url = "https://swapi.dev/api"
     url = f"{base_url}{resource}?page=1"
     list_of_elements = []
@@ -20,7 +28,16 @@ def get_list_all_elements(resource: str) -> list[dict]:
     return list_of_elements
 
 
-def export_generic_model_to_csv(model_class: BaseModel, list_of_elements_to_be_exported: list[dict], exported_file_name: str) -> str:
+def export_generic_model_to_csv(model_class: BaseModel, list_of_elements_to_be_exported: list[dict], exported_file_name: str):
+    """Carries out export of containing values in the given list of elements, using the given Pydantic model
+    to ensure the output format.     
+
+    Args:
+        model_class (BaseModel): A pydantic BaseModel of the given list_of_elements modules
+        list_of_elements_to_be_exported (list[dict]): A list that be taken from `get_list_all_elements` function
+        exported_file_name (str): The name of file that will be exported.
+
+    """
     with open(exported_file_name, "w", newline='') as myfile:
         writer = csv.DictWriter(myfile, fieldnames=model_class.model_fields.keys())
         writer.writeheader()
