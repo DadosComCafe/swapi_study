2b0ef9e18a951f02712839d6a7f536839030779b
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-12-01 09:36:14 -0300

    (search.py): Finalizing count people with more than 2 starships

--- search.py
+++ search.py
@@ -4,6 +4,15 @@ def count_distinct_values(field_name: str, csv_name: str) -> int:
     df = pd.read_csv(csv_name)
     return len(pd.unique(df[field_name]))
 
+
+def count_people_with_more_than_two_elements(field_name: str, csv_name: str) -> int:
+    df = pd.read_csv(csv_name)
+    return len(df[df[field_name].map(len) > 2])
+
+
 if __name__ == "__main__":
     # how many starships?
     print(count_distinct_values("name", "starships.csv"))
+
+    # how many people with more than 2 starships?
+    print(count_people_with_more_than_two_elements("starships", "personagens.csv"))
